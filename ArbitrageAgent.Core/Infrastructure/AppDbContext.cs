ed2d7ada1830730c9cdc8b254a3046c349d6f536
Nadav <nro008@gmail.com>
2025-12-27 11:21:11 +0200

    thread safe dbcontext

--- ArbitrageAgent.Core/Infrastructure/AppDbContext.cs
+++ ArbitrageAgent.Core/Infrastructure/AppDbContext.cs
@@ -6,28 +6,46 @@ namespace ArbitrageAgent.Core.Infrastructure
     public class AppDbContext
     {
         private readonly SQLiteAsyncConnection _db;
+        private readonly SemaphoreSlim _semaphore = new(1, 1);
+        private readonly Task _initializationTask;
 
         public AppDbContext(string dbPath)
         {
             _db = new SQLiteAsyncConnection(dbPath);
-            _db.CreateTableAsync<Asset>().Wait();
-            _db.CreateTableAsync<Exchange>().Wait();
-            _db.CreateTableAsync<Settings>().Wait();
+            _initializationTask = InitializeTables();
+        }
+        public async Task InitializeTables()
+        {
+            await ExecuteAction(() => _db.CreateTablesAsync<Asset, Exchange, Settings>());
+        }
+        public async Task<T> ExecuteAction<T>(Func<Task<T>> action)
+        {
+            await _initializationTask;
+            await _semaphore.WaitAsync();
+
+            try
+            {
+                return await action();
+            }
+            finally
+            {
+                _semaphore.Release();
+            }
         }
 
         public async Task ResetAsync()
         {
-            await _db.DeleteAllAsync<Asset>().ConfigureAwait(false);
-            await _db.DeleteAllAsync<Exchange>().ConfigureAwait(false);
-            await _db.DeleteAllAsync<Settings>().ConfigureAwait(false);
+            await ExecuteAction(() => _db.DeleteAllAsync<Asset>());
+            await ExecuteAction(() => _db.DeleteAllAsync<Exchange>());
+            await ExecuteAction(() => _db.DeleteAllAsync<Settings>());
         }
 
-        public async Task<List<Asset>> GetAssetsAsync() => await _db.Table<Asset>().ToListAsync();
-        public async Task<List<Exchange>> GetExchangesAsync() => await _db.Table<Exchange>().ToListAsync();
-        public async Task<List<Settings>> GetSettingsAsync() => await _db.Table<Settings>().ToListAsync();
-        public async Task<int> AddAsync<T>(T item) => await _db.InsertAsync(item);
-        public async Task<int> SaveAsync<T>(T item) => await _db.InsertOrReplaceAsync(item);
-        public async Task<int> DeleteAsync<T>(T item) => await _db.DeleteAsync(item);
+        public async Task<List<Asset>> GetAssetsAsync() => await ExecuteAction(() => _db.Table<Asset>().ToListAsync());
+        public async Task<List<Exchange>> GetExchangesAsync() => await ExecuteAction(() => _db.Table<Exchange>().ToListAsync());
+        public async Task<List<Settings>> GetSettingsAsync() => await ExecuteAction(() => _db.Table<Settings>().ToListAsync());
+        public async Task<int> AddAsync<T>(T item) => await ExecuteAction(() => _db.InsertAsync(item));
+        public async Task<int> SaveAsync<T>(T item) => await ExecuteAction(() => _db.InsertOrReplaceAsync(item));
+        public async Task<int> DeleteAsync<T>(T item) => await ExecuteAction(() => _db.DeleteAsync(item));
     }
 
 }
